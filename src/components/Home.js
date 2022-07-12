aefeeb4880e38db8f9b63787228effb70a5be39e
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2022-07-12 09:15:05 +0100

    button added in projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,6 +33,9 @@ import { DiPostgresql } from 'react-icons/di'
 import { DiGithubBadge } from 'react-icons/di'
 import { DiHtml5 } from 'react-icons/di'
 import { DiCss3 } from 'react-icons/di'
+import { DiGitBranch } from 'react-icons/di'
+
+
 
 import { MdOutlineEmail } from 'react-icons/md'
 import { Rotate, LightSpeed } from 'react-reveal'
@@ -40,6 +43,12 @@ import { Rotate, LightSpeed } from 'react-reveal'
 import Jump from 'react-reveal'
 import { Container } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
+
+// import ReactTooltip from 'react-tooltip'
+import Tippy from '@tippy.js/react'
+import 'tippy.js/dist/tippy.css'
+
+
 
 
 
@@ -67,11 +76,11 @@ const Home = () => {
         </LightSpeed>
         <Jump bottom>
 
-          <p className='text-center mt-3 mr-5  '> Hi! 你好, I&apos;m a Full-stack Wed Developer and a recent software engineering graduate from General Assembly (June 2022).
-            Current Stack: HTML, CSS, JavaScript, React.js, Express.js, Python, Django.<br></br>I’m a nice, fun and friendly person, I’m honest and punctual, I work well in a team but also on my own and I have good listening and communication skills.
-          <br></br> In addition, being a mother has helped me to go through this course even though it was very challenging, as I wanted to set an example for my daughter so that she would not give up on her dreams.
-            Now that I have completed the course, I’d like to find a place where I can work in a trusting, goal-oriented, and supportive environment. </p>
-          <p className='text-center mt-3  big'> </p>
+          <p className='text-center mt-3 mr-5 text-md-left '> Hi! 你好, I&apos;m a Full-stack Wed Developer and a recent software engineering graduate from General Assembly (June 2022).<br></br>
+            I’m a nice, fun and friendly person, I’m honest and punctual, I work well in a team but also on my own and I have good listening and communication skills.<br></br>
+
+            Now that I have completed the course, I’d like to find a place where I can work in a trusting, goal-oriented, and supportive environment.<br></br> Current Stack: HTML, CSS, JavaScript, React.js, Express.js, Python, Django. </p>
+
 
         </Jump>
 
@@ -92,18 +101,40 @@ const Home = () => {
           <div className='project-body odd'>
             <h2>My Kitchen</h2>
             <h3>1 week | Solo</h3>
-            <div>
-              <SiReact />
-              <IoLogoJavascript />
-              <SiDjango />
-              <IoLogoPython />
-              <IoLogoSass />
-              <SiPostgresql />
+            <div className='iconsdiv'>
+
+              <Tippy content='React' placement='bottom'>
+                <button><SiReact /></button>
+              </Tippy>
+
+
+
+
+              <Tippy content='Javascript' placement='bottom'>
+
+                <button><IoLogoJavascript /></button>
+              </Tippy>
+
+              <Tippy content='Diango' placement='bottom'>
+
+                <button> <SiDjango /></button>
+              </Tippy>
+              <Tippy content='Python' placement='bottom'>
+
+                <button><IoLogoPython /></button>
+              </Tippy>
+              <Tippy content='Sass' placement='bottom'>
+
+                <button><IoLogoSass /></button>
+              </Tippy>
+
+
             </div>
+
             <p>
-            A Full-stack web application that allows users to view the menu and the location of the different restaurants and to make a booking from the restaurant. A confirmation email will be sent to users after the booking is made, and users can log in to their account to see the booking details and delete or modify the booking if needed.
+              A Full-stack web application that allows users to view the menu and the location of the different restaurants and to make a booking from the restaurant. A confirmation email will be sent to users after the booking is made, and users can log in to their account to see the booking details and delete or modify the booking if needed.
             </p>
-            <div>
+            <div className=''>
               <a href="https://mykitchenproject4.herokuapp.com/" target="_blank" rel="noreferrer">
                 <FaExternalLinkSquareAlt />
               </a>
@@ -117,13 +148,19 @@ const Home = () => {
           <div className='project-body even'>
             <h2>Frog Vs Mario</h2>
             <h3>1 week | Solo</h3>
-            <div>
-              <AiFillHtml5 />
-              <IoLogoCss3 />
-              <IoLogoJavascript />
+            <div className='iconsdiv'>
+              <Tippy content='Html' placement='bottom'>
+                <button><AiFillHtml5 /></button>
+              </Tippy>
+              <Tippy content='Css' placement='bottom'>
+                <button><IoLogoCss3 /></button>
+              </Tippy>
+              <Tippy content='Javascript' placement='bottom'>
+                <button> <IoLogoJavascript /></button>
+              </Tippy>
             </div>
             <p>
-             
+
               This is a personalised version of the famous arcade game Frogger. Features include sound effects, six lives to reach the finish line.
             </p>
             <div>
@@ -142,18 +179,31 @@ const Home = () => {
           <div className='project-body odd'>
             <h2>Fruity 4 You</h2>
             <h3>36 hours | Group Project</h3>
-            <div>
-              <SiReact />
-              <IoLogoJavascript />
-              <IoLogoCss3 />
-              <SiBootstrap />
+            <div className='iconsdiv'>
+
+              <Tippy content='Javascript' placement='bottom'>
+                <button><SiReact /></button>
+              </Tippy>
+
+              <Tippy content='Javascript' placement='bottom'>
+
+                <button> <IoLogoJavascript /></button>
+              </Tippy>
+
+              <Tippy content='Javascript' placement='bottom'>
+                <button><IoLogoCss3 /></button>
+              </Tippy>
+
+              <Tippy content='Javascript' placement='bottom'>
+                <button><SiBootstrap /></button>
+              </Tippy>
             </div>
             <p>
-            Working in a 3-person group one-day hackathon project. We built an app that consumes a public API (fruityvice), allowing users to search
+              Working in a 3-person group one-day hackathon project. We built an app that consumes a public API (fruityvice), allowing users to search
               all different kinds of fruits by either name or family. Users can also calculate nutrition after choosing three of their favourite fruits.
             </p>
-            <div >
-              <a className='mt-5' href="https://fruity-4-you.netlify.app/" target="_blank" rel="noreferrer">
+            <div className='mt-5'>
+              <a href="https://fruity-4-you.netlify.app/" target="_blank" rel="noreferrer">
                 <FaExternalLinkSquareAlt />
               </a>
               <a href="https://github.com/liuyuanmeng/Project2-FruitsHackathon" target="_blank" rel="noreferrer">
@@ -166,19 +216,32 @@ const Home = () => {
           <div className='project-body even'>
             <h2>Book Store</h2>
             <h3>1 week | Group Project</h3>
-            <div>
-              <SiReact />
-              <IoLogoJavascript />
-              <SiMongodb />
-              <IoLogoNodejs />
-              <IoLogoSass />
-              <SiPostgresql />
+            <div className='iconsdiv'>
+              <Tippy content='React' placement='bottom'>
+                <button><SiReact /></button>
+              </Tippy>
+              <Tippy content='Javascript' placement='bottom'>
+                <button><IoLogoJavascript /></button>
+              </Tippy>
+              <Tippy content='Mongodb' placement='bottom'>
+                <button><SiMongodb /></button>
+              </Tippy>
+              <Tippy content='Node.js' placement='bottom'>
+                <button> <IoLogoNodejs /></button>
+              </Tippy>
+              <Tippy content='Sass' placement='bottom'>
+                <button><IoLogoSass /></button>
+              </Tippy>
+              <Tippy content='PostgreSQL' placement='bottom'>
+                <button> <SiPostgresql /></button>
+              </Tippy>
+
             </div>
             <p>
-            As a group of three people, we created a book app 
+              As a group of three people, we created a book app
               which allows users to register and interact by posting reviews, updating account information, and adding books to the
               wishlist. Full-Stack MERN app with a CRUD API that consumes a MongoDB database.
-              
+
             </p>
             <div>
               <a href="https://firestonebookshop.herokuapp.com/" target="_blank" rel="noreferrer">
@@ -221,7 +284,7 @@ const Home = () => {
             </div>
             <div className='icon-item'>
 
-          
+
 
               <p>React</p>
               <FaReact style={{ fontSize: '50px' }} />
@@ -265,6 +328,12 @@ const Home = () => {
 
               <p>GitHub</p>
               <DiGithubBadge style={{ fontSize: '50px' }} />
+            </div>
+
+            <div className='icon-item'>
+
+              <p>Git</p>
+              <DiGitBranch style={{ fontSize: '50px' }} />
             </div>
 
             <div className='icon-item'>
